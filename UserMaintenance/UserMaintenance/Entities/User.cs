d486a3e439a893ddcbfcce0ce2fea5356cee6526
VillyX <villyx@gmail.com>
2022-10-08 14:52:40 +0200

    feladat6

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get //csak get át van, hogy írásvédett legyen
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+    }
+}
