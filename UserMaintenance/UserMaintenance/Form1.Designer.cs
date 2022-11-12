d71abec5a41834ca161ee6b92896abe009fb679d
Bence Viola <villyx@gmail.com>
2022-11-12 01:02:04 +0100

    save to file

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace UserMaintenance
             this.lblFullName = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnWriteToFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -73,11 +74,22 @@ namespace UserMaintenance
             this.textBox1.Size = new System.Drawing.Size(114, 27);
             this.textBox1.TabIndex = 4;
             // 
+            // btnWriteToFile
+            // 
+            this.btnWriteToFile.Location = new System.Drawing.Point(360, 338);
+            this.btnWriteToFile.Name = "btnWriteToFile";
+            this.btnWriteToFile.Size = new System.Drawing.Size(94, 29);
+            this.btnWriteToFile.TabIndex = 5;
+            this.btnWriteToFile.Text = "button2";
+            this.btnWriteToFile.UseVisualStyleBackColor = true;
+            this.btnWriteToFile.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 444);
+            this.Controls.Add(this.btnWriteToFile);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblFullName);
@@ -96,6 +108,7 @@ namespace UserMaintenance
         private System.Windows.Forms.Label lblFullName;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnWriteToFile;
     }
 }
 
