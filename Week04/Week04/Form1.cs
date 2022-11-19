8e846d637d5987d3a48dee560c6d0c020a09e656
VillyX <villyx@gmail.com>
2022-11-19 18:15:34 +0100

    feladat 8

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -88,8 +88,31 @@ namespace Week04
             r = xlSheet.get_Range(GetCell(2, 1), GetCell(flats.Count + 1, headers.Length)); //7.8
             r.Value = values;
             r = xlSheet.get_Range(GetCell(2, 9),
-                        GetCell(flats.Count, 9));
+                        GetCell(flats.Count+1, 9));
             r.Value = "=1000000*"+GetCell(2,8)+"/"+GetCell(2,7);
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length)); //8.1
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            r = xlSheet.UsedRange; //8.2
+            r.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            r = xlSheet.get_Range(GetCell(2, 1),
+                GetCell(flats.Count+1, 1));
+            r.Font.Bold = true;
+            r.Interior.Color = Color.LightYellow;
+
+            r = xlSheet.get_Range(GetCell(2, 9), //8.2 saját kútőből
+                GetCell(flats.Count+1, 9));
+            r.Interior.Color = Color.LightGreen;
+            r.NumberFormat = "0.00";
+
+
         }
         private string GetCell(int x, int y) //7.7
         {
