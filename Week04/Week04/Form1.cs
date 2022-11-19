09fc3add5e4a5966e8277f1afa298f14a49e8781
VillyX <villyx@gmail.com>
2022-11-19 17:30:47 +0100

    feladat 6

--- Week04/Week04/Form1.cs
+++ Week04/Week04/Form1.cs
@@ -16,11 +16,45 @@ namespace Week04
     {
         List<Flat> Flats; //4.1
         RealEstateEntities re = new RealEstateEntities(); //4.2
-        
+
+        // 6.1 feladat
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         void LoadData() //4.3
         {
             Flats = re.Flat.ToList(); //4.4
         }
+
+        void CreateExcel() //6.2
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable(); //majd ez tölti fel adatokkal
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Source + '\n' + ex.Message);
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        private void CreateTable()
+        {
+            
+
+        }
+
         public Form1()
         {
             InitializeComponent();
