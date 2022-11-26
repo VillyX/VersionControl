0b5fbed30295f3cc07f4d05c368a0ad651343527
VillyX <villyx@gmail.com>
2022-11-26 09:18:19 +0100

    esponse feladat

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.MnbServiceReference;
 
 namespace week05
 {
@@ -15,6 +16,19 @@ namespace week05
         public Form1()
         {
             InitializeComponent();
+
+
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames ="EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            MessageBox.Show(result);
         }
     }
 }
