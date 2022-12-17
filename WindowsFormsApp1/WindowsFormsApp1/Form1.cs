87d7bba041642342fda52ddebb1fd63cc223c5c1
VillyX <villyx@gmail.com>
2022-12-17 13:55:03 +0100

    feladat 5

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace WindowsFormsApp1
@@ -15,14 +16,24 @@ namespace WindowsFormsApp1
     { 
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities(); //4-es feladat
-
-
+        List<PortfolioItem> portfolio = new List<PortfolioItem>();
     
         public Form1()
         {
             InitializeComponent();
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks; //a ticks lista (5-ös feladat)
+
+            CreatePortfolio(); //nem fgv, hanem eljárás a visszaadott érték hiánya miatt (5-ös feladat még mindig)
+        }
+
+        private void CreatePortfolio()
+        {
+            portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = portfolio;
         }
     }
 }
